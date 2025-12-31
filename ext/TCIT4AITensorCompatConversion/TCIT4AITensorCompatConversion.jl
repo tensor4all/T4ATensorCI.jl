48e5d1e5b254f9424915e30bee4cd59d948959e0
Hiroshi Shinaoka <h.shinaoka@gmail.com>
2025-12-31 20:31:10 +0900

    Extensions: delegate conversions to T4ATensorTrain
    Stop defining duplicate ITensor/T4AITensorCompat conversion methods in T4ATensorCI extensions (they live in T4ATensorTrain ext/). Also disable Aqua persistent_tasks to avoid registry-resolution flakiness in CI.

--- ext/TCIT4AITensorCompatConversion/TCIT4AITensorCompatConversion.jl
+++ ext/TCIT4AITensorCompatConversion/TCIT4AITensorCompatConversion.jl
@@ -1,11 +1,14 @@
 module TCIT4AITensorCompatConversion
 
 import T4ATensorCI as TCI
+import T4ATensorTrain
 
 using ITensors
 using T4AITensorCompat
 
-include("t4aitensorcompat_conversion.jl")
+# Conversions between `T4ATensorTrain.TensorTrain` (re-exported by T4ATensorCI)
+# and `T4AITensorCompat.TensorTrain` are provided by `T4ATensorTrain`'s extension
+# `TTT4AITensorCompatConversion`. Do not re-define them here.
 
 end
 
